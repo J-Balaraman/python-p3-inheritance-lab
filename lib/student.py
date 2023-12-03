295085fca815852da9f9e9bed463f88fa32a33a7
jbalaraman <james.balaraman@gmail.com>
2023-12-02 19:02:25 -0700

    Beans

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
+    def __init__(self, first_name, last_name, knowledge=[]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+        #self.additional_knowledge = additional_knowledge
+        
+    def learn(self, extra):
+        self.knowledge.append(extra)
         pass
